f8295dab20b4cc2e9e8901dc29e72098c4d386d6
TeamKodiAddonsPT <j2mf42@gmail.com>
2019-07-23 20:31:24 +0100

    .

--- plugin.video.m.team/_Edit.py
+++ plugin.video.m.team/_Edit.py
@@ -1,4 +1,4 @@
 import xbmcaddon
 
-MainBase = 'aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3NsYmZvcmV2ZXJzbGIvbGlrZXRoaXMvbWFzdGVyL29uZQ=='.decode('base64')
+MainBase = 'aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL21hY2tsYW5kcm8vZnJlZTRhbGwvbWFzdGVyL2ZpcnN0'.decode('base64')
 addon = xbmcaddon.Addon('plugin.video.m.team')
